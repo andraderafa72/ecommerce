1ed606a8a40c75cc76eaec03bd2a81de1a46f998
Rafael Andrade <devrafael72@gmail.com>
2021-09-01 22:06:40 -0300

    Ajustes na página de produto

--- assets/js/produto.js
+++ assets/js/produto.js
@@ -1,36 +1,53 @@
-var swiperThumbs = new Swiper('.slide-preview .swiper-thumbs', {
-  loop: true,
-})
+var swiperThumbs = new Swiper(".slide-preview .swiper-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 4, // DEFINIR COMO O NUMERO DE IMAGENS DO PRODUTO
+  freeMode: true,
+  watchSlidesProgress: true,
+});
 
-var productSwiper = new Swiper('.product-swiper .swiper', {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination-product",
-  },
+var productSwiper = new Swiper(".product-swiper .swiper", {
+  centeredSlides: true,
+  spaceBetween: 10,
   navigation: {
-    nextEl: '.swiper-button-next-product',
-    prevEl: '.swiper-button-prev-product',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  spaceBetween: 30,
-  // thumbs: {
-  //   swiper: swiperThumbs
-  // }
+  thumbs: {
+    swiper: swiperThumbs,
+  },
 });
 
 const variants = document.querySelectorAll('input[name="variant"]');
 
-variants.forEach(el =>{
-  el.addEventListener('click', e => {
+const plusButton = document.querySelector(".plus");
+const minusButton = document.querySelector(".minus");
+const quantityInput = document.querySelector("#quantity");
+
+plusButton.addEventListener("click", (e) => {
+  quantityInput.value = Number(quantityInput.value) + 1;
+});
+
+minusButton.addEventListener("click", (e) => {
+  quantityInput.value =
+    Number(quantityInput.value) - 1 < 1 ? 1 : Number(quantityInput.value) - 1;
+});
+
+variants.forEach((el) => {
+  el.addEventListener("click", (e) => {
     el.checked = true;
 
-    variants.forEach(variant => {
-      variant.checked = false
-      const variantLabel = document.querySelector(`label[for=${variant.attributes.id.nodeValue}]`)
-      variantLabel.classList.remove('active');
+    variants.forEach((variant) => {
+      variant.checked = false;
+      const variantLabel = document.querySelector(
+        `label[for=${variant.attributes.id.nodeValue}]`
+      );
+      variantLabel.classList.remove("active");
     });
 
-    const label = document.querySelector(`label[for=${el.attributes.id.nodeValue}]`)
+    const label = document.querySelector(
+      `label[for=${el.attributes.id.nodeValue}]`
+    );
 
-    label.classList.add('active');
-  })
-})
+    label.classList.add("active");
+  });
+});
