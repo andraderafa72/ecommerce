c4c9402f66c5e1c9d7f98ecca3cfd37abbb30471
Rafael Andrade <devrafael72@gmail.com>
2021-09-09 16:20:13 -0300

    Inserção de underlay

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -1,4 +1,8 @@
 const categoriesMenu = document.querySelector(".categories-header-menu");
+
+// const categoriesHoverMenuButton = document.querySelector(".side-categories-menu-toggle");
+const burgerUnderlay = document.querySelector(".burger-underlay");
+
 const toggleCategoriesMenu = document.querySelector(".toggle-categories-menu");
 
 const burgerCollapse = document.querySelector(".hamburger--collapse");
@@ -15,9 +19,23 @@ toggleCategoriesMenu.addEventListener("click", (e) => {
 burgerCollapse.addEventListener("click", (e) => {
   if (burgerCollapse.classList.contains("is-active")) {
     burgerMenu.classList.remove('visible')
+    burgerUnderlay.classList.remove('visible')
     burgerCollapse.classList.remove("is-active");
   } else {
     burgerCollapse.classList.add("is-active");
+    burgerMenu.classList.add('visible')
+    burgerUnderlay.classList.add('visible')
+  }
+});
+
+burgerUnderlay.addEventListener("click", (e) => {
+  if (burgerCollapse.classList.contains("is-active")) {
+    burgerMenu.classList.remove('visible')
+    burgerCollapse.classList.remove("is-active");
+    burgerUnderlay.classList.remove('visible')
+  } else {
+    burgerCollapse.classList.add("is-active");
+    burgerUnderlay.classList.add('visible')
     burgerMenu.classList.add('visible')
   }
 });
