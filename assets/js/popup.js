8a8fa664b9be666ac4c3423357e6ce0697a23460
Rafael Andrade <devrafael72@gmail.com>
2021-09-18 12:13:38 -0300

    Produção

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,8 +1,19 @@
-var closePopup = document.querySelector('.cupom-popup-wrapper .cupoms button');
-var popupScreen = document.querySelector('.cupom-popup-wrapper');
+var closePopup = document.querySelector(".cupom-popup-wrapper .cupoms button");
+var popupScreen = document.querySelector(".cupom-popup-wrapper");
 closePopup.addEventListener("click", (e) => {
-  popupScreen.classList.add('hidden');
+  popupScreen.classList.add("hidden");
   setTimeout(() => {
-    popupScreen.classList.add('negative-index');
-  }, 1000)
-})
+    popupScreen.classList.add("negative-index");
+  }, 1000);
+});
+
+popupScreen.addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (target === popupScreen) {
+    popupScreen.classList.add("hidden");
+    setTimeout(() => {
+      popupScreen.classList.add("negative-index");
+    }, 1000);
+  }
+});
